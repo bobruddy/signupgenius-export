05fc4ca6e117245ea31e0d681ca1376556fae616
Robert Ruddy <bob@ruddy.net>
2026-02-19 21:30:19 -0500

    Resolve merge conflict in setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,15 @@
 from setuptools import setup, find_packages
 
-with open("requirements.txt") as f:
-    requirements = f.read().splitlines()
 
 setup(
     name="signupgenius-export",
-<<<<<<< HEAD
     version="1.0.2",
-=======
     version="1.0.1",
->>>>>>> 6a1fd14 (Fix setup.py to hardcode requirements, update author)
     description="Export SignUpGenius signup data to CSV",
     author="Robert Ruddy",
     packages=find_packages(),
     py_modules=["export_signup"],
-    install_requires=requirements,
+    install_requires=["requests"],
     license="GPL-3.0-only", 
     entry_points={
         "console_scripts": [
